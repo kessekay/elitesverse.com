d544bbfff0ad259d783e4531d1f5b35e48319d52
Kelvin Kesse - Kobbina <kelvin@kessekobbina.com>
2023-06-03 12:38:33 +0000

    updated terms and privacy policy

--- pages/privacy.tsx
+++ pages/privacy.tsx
@@ -1,8 +1,66 @@
-import React from 'react'
+import React from 'react';
+import Container from '../components/container';
 
 const privacy = () => {
   return (
-    <div>privacy</div>
+    <div>
+      <Container>
+        <div className="my-8 text-lg">
+          <h1 className=" text-3xl font-bold mb-4">Privacy Policy</h1>
+
+          <h2 className="mb-8 text-2xl font-semibold">Last Updated: Saturday, 3 June 2023</h2> <br />
+
+          This Privacy Policy outlines the types of personal information collected, used, and shared by ElitesVerse ("we," "us," or "our") and describes how we safeguard and protect your privacy. By accessing and using elitesverse.com, you consent to the terms and practices described in this Privacy Policy. <br />
+          <br />
+
+          <p className=" leading-10">
+            1. Information Collection <br />
+            
+            1.1. Personal Information <br />
+            We may collect personal information that you voluntarily provide to us when you interact with ElitesVerse. This information may include your name, email address, and any other information you choose to provide when submitting comments or contacting us. <br />
+
+            1.2. Log Data <br />
+            Like many websites, we collect log data, including your IP address, browser type, operating system, referring URLs, and the date and time of your visit. This information is used for statistical purposes and to improve the functionality and performance of ElitesVerse. <br />
+
+            2. Cookies and Tracking Technologies <br />
+
+            2.1. Cookies <br />
+            We use cookies and similar tracking technologies to enhance your browsing experience on ElitesVerse. Cookies are small files that are stored on your device and enable us to recognize your browser or device and remember certain information. You have the option to disable cookies through your browser settings, but this may limit certain features of the website. <br />
+
+            2.2. Third-Party Analytics <br />
+            We may use third-party analytics services, such as Google Analytics, to gather and analyze information about how users interact with ElitesVerse. These analytics providers may use cookies or other tracking technologies to collect information such as your IP address, browser information, and website usage patterns. The data collected by these services is used to improve the website's functionality and understand user preferences. <br />
+
+            3. Use of Information <br />
+
+            3.1. Personal Information <br />
+            We may use the personal information you provide to respond to your inquiries, communicate with you, and send you relevant information or updates related to ElitesVerse. We will not share your personal information with third parties for direct marketing purposes without your explicit consent. <br />
+
+            3.2. Log Data and Analytics<br />
+            The log data and analytics information collected are used to analyze trends, administer the website, track user movements, and gather demographic information for internal purposes. This information helps us improve the content, design, and functionality of ElitesVerse and provide a better user experience. <br />
+
+            4. Information Sharing <br />
+
+            4.1. Third-Party Service Providers <br />
+            We may engage third-party service providers to assist us in operating and maintaining ElitesVerse, including but not limited to website hosting, maintenance, and analytics. These providers may have access to your personal information only to perform their tasks on our behalf and are obligated not to disclose or use it for any other purpose. <br />
+
+            4.2. Legal Compliance and Protection <br />
+            We may disclose your personal information if required by law, regulation, or legal process, or if we believe it is necessary to protect our rights, property, or safety, or that of others. <br />
+
+            5. Data Security <br />
+
+            We take reasonable precautions to protect the security of your personal information. However, please be aware that no method of transmission over the internet or electronic storage is completely secure. We cannot guarantee the absolute security of your data. <br />
+
+            6. Children's Privacy <br />
+
+            ElitesVErse is not intended for individuals under the age of 13. We do not knowingly collect personal information from children. If you are a parent or guardian and believe that your child has provided us with personal information, please contact us, and we will take steps to remove that information from our records. <br />
+
+            7. Third-Party Links <br />
+          </p>
+
+          ElitesVerse may contain links to third-party websites that are not operated or controlled by us. We are not responsible for the privacy practices of these third-party websites. We encourage
+        </div>
+      </Container>
+    </div>
   )
 }
 
--- pages/terms.tsx
+++ pages/terms.tsx
@@ -1,8 +1,67 @@
 import React from 'react';
+import Container from '../components/container';
 
 const terms = () => {
   return (
-    <div>terms</div>
+    <div className="mt-8">
+      <Container>
+        <div className="my-8 text-lg">
+          <h1 className=" text-3xl font-bold mb-4">Terms and Conditions</h1>
+
+          <h2 className="mb-8 text-2xl font-semibold">Last Updated: Saturday, June 2023</h2>
+
+          <div className="leading-10">
+
+            Please read these Terms and Conditions carefully before using ElitesVerse. These terms govern your access to and use of ElitesVerse, including any content, features, or services offered on or through the website. <br />
+
+            1. Acceptance of Terms <br />
+
+            By accessing or using ElitesVerse, you agree to be bound by these Terms and Conditions and any additional terms and policies incorporated by reference. If you do not agree to these terms, please refrain from using ElitesVerse. <br />
+
+            2. Intellectual Property <br />
+
+            2.1. Content Ownership <br />
+            All content on ElitesVerse, including articles, blog posts, images, graphics, logos, and other materials, is owned or licensed by ElitesVerse. This content is protected by copyright, trademark, and other intellectual property laws. You may not modify, reproduce, distribute, or exploit any content without prior written permission from Elitesverse. <br />
+
+            2.2. User-Generated Content <br />
+            By posting, submitting, or otherwise providing any content on ElitesVerse, you grant us a non-exclusive, royalty-free, worldwide, perpetual, and irrevocable license to use, display, modify, distribute, and sublicense the content. You represent and warrant that you own or have the necessary rights to grant this license and that your content does not infringe upon the rights of any third party. <br />
+
+            3. User Responsibilities <br />
+
+            3.1. Account Registration <br />
+            Certain features or services on ElitesVerse may require you to create an account. You are responsible for maintaining the confidentiality of your account credentials and for any activities that occur under your account. You agree to provide accurate and up-to-date information during the registration process and to promptly update any changes. <br />
+
+            3.2. Prohibited Activities <br />
+            When using ElitesVerse, you agree not to: <br />
+
+            <ul> <br />
+              <li>Violate any applicable laws, regulations, or third-party rights.</li>
+              <li>Impersonate any person or entity or falsely represent your affiliation with any person or entity.</li>
+              <li>Engage in any form of fraudulent, abusive, or illegal activity.</li>
+              <li>Interfere with or disrupt the operation or security of ElitesVerse or any related systems or networks.</li>
+              <li>Use any automated means, including bots, spiders, or scrapers, to access or collect information from ElitesVerse without our prior written consent.</li>
+              <li>Upload or transmit any viruses, malware, or other harmful code that may damage or interfere with ElitesVerse or the data of other users.</li>
+            </ul>
+
+            4. Disclaimer of Warranties <br />
+
+            ElitesVerse and its content are provided on an "as is" and "as available" basis without warranties of any kind. We do not guarantee the accuracy, completeness, or reliability of any content or information provided on ElitesVerse. Your use of ElitesVerse is at your own risk. <br />
+
+            5. Limitation of Liability <br />
+
+            To the maximum extent permitted by law, ElitesVerse and its owners, directors, employees, and affiliates shall not be liable for any direct, indirect, incidental, consequential, or special damages arising out of or in any way connected with your use of ElitesVerse or reliance on any content provided. <br />
+
+            6. Modification and Termination <br />
+
+            We reserve the right to modify or terminate ElitesVerse, its content, or any features or services at any time without prior notice. We may also revise these Terms and Conditions by updating this page. Continued use of ElitesVerse after any changes shall constitute your acceptance of the revised terms. <br />
+
+            7. Governing Law and Jurisdiction <br />
+
+            These Terms and Conditions shall be governed by and construed in accordance with the laws of ElitesVerse. <br />
+          </div>
+        </div>
+      </Container>
+    </div>
   )
 }
 
